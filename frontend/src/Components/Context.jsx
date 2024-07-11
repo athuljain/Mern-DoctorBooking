67246e8b4561736c7ff31e6bc2acd6a109210dd8
athuljain <athuljainkj2@gmail.com>
2024-07-11 10:38:42 +0530

    frontend setups

--- frontend/src/Components/Context.jsx
+++
@@ -1,6 +0,0 @@
-
-import { createContext } from "react";
-
-export const mycontext = createContext(); // Exporting the context
-
-export const MyProvider = mycontext.Provider; // Exporting the provider
